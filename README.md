d495ed88d9c2a8973bad5d8cd3c8ba61140dfe82
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2022-09-19 22:29:54 +0100

    Add images and annimations

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # microverse-capstone
+ This project consist of a combination of all the topics I learned, being my first month as a microverse student. I  built a web page for a conference that supports celebration, believing in the value of openness and sharing. In creating a positive change working with varieties of people from different countries. The following were applied here:
+-I used semantic HTML tags in this project.
+-I used CSS selectors correctly.
+-I Used CSS box model.
+-I used Flexbox to place elements in the page.
+-I Demonstrated the ability to create UIs adaptable to different screen sizes using media queries.
+-I used GitHub Pages to deploy web pages.
+-I Applied JavaScript best practices and language style guides in this code.
+-I Used JavaScript to manipulate DOM elements.
+-I Used JavaScript events.
+-I used objects to store and access data.
+## Built With
+
+- HTML for layout 
+- CSS for styling
+- Linters HTML/CSS and Javascript
+-Javascript for language
+
+## Live Demo (if available)
+
+
+## Getting Started
+To get a local copy
+
+### Prerequisites
+
+### Setup
+
+### Install
+
+### Usage
+
+### Run tests
+
+### Deployment
+
+
+## Authors
+
+👤 **Ngufor emmanuella**
+
+- GitHub: [@Ngufor-emmanuella](https://github.com/Ngufor-emmanuella)
+- Twitter: [@NguforEmmanuella](https://twitter.com/NguforEmmanuella)
+- LinkedIn: [Nguforemmanuella](https://linkedin.com/in/Nguforemmanuella)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ index.js
@@ -0,0 +1,144 @@
+const ham = document.getElementById('ham');
+const mobileNav = document.getElementById('hide')
+ham.addEventListener('click', () => {
+  mobileNav.style.left = '0';
+ mobileNav.style.right = '0';
+});
+
+// const links =document.querySelectorAll('.navlink');
+// links.forEach((link) => {
+//   link.addEventListerner('click', () => {
+//     div.classList.add('hide');
+//     ham.classList.remove('hide');
+//     document.querySelector('.box').classList.remove('blur');
+//   })
+// })
+
+// This will listen for clicks on the X button
+const closebtn = document.querySelector('.markx');
+closebtn.addEventListener('click', () => {
+ mobileNav.style.left = '-100vw';
+ mobileNav.style.right = '100vw';
+})
+/* main program zone */
+let data =[
+  {
+    image:'fa-solid fa-masks-theater',
+    heading:'healthy',
+    paragraph:'Appreciate various creations applying CC license of researchers, organised in the country.'
+  },
+  {
+    image:'fa-solid fa-masks-theater',
+    heading:"Long life",
+    paragraph:'Appreciate various creations applying CC license of researchers, organised in the country.'
+  },
+  {
+    image:'fa-solid',
+    heading:"healthy",
+    paragraph:'Appreciate various creations applying CC license of researchers, organised in the country.'
+  },
+  {
+    image:'fa-solid fa-masks-theater',
+    heading:'Long life',
+    paragraph:'Appreciate various creations applying CC license of researchers, organised in the country.'
+  },
+  {
+    image:'fa-solid',
+    heading:"healthy",
+    paragraph:'Appreciate various creations applying CC license of researchers, organised in the country.'
+  },
+];
+const mainProHead = document.querySelector('#procarthead');
+mainProHead.innerHTML='Main Program';
+function Cartdisplay() {
+  for (let i = 0; i < data.length; i += 1) {
+    const mainProContent = document.getElementById('cartContainer');
+    const cart = document.createElement("div");
+    cart.classList.add('box2a');
+    const divImg = document.createElement("div");
+    divImg.classList.add('box2a-img');
+    const iconImg = document.createElement("i");
+    iconImg.classList.add('fa-solid');
+    iconImg.classList.add('fa-masks-theater');
+    const cartHead = document.createElement("h2");
+    cartHead.innerHTML=data[i].heading;
+    const cartPara = document.createElement("p");
+    cartPara.innerHTML = data[i].paragraph;
+    /* Need to append all the element to it various parent */
+    mainProContent.appendChild(cart);
+    cart.appendChild(divImg);
+    divImg.appendChild(iconImg);
+    cart.appendChild(cartHead);
+    cart.appendChild(cartPara);
+
+  }
+}
+Cartdisplay(data);
+let featureData = [
+  {
+    image: 'images/cucumber.jpg',
+    head:'Benefits of cucumbers',
+    subHead:'Research has proven that some fruits and veggies reduces blood sugar.',
+    para:'Come learn the benefits cucumers has in reducing blood sugar spikes... '
+  },
+  {
+    image: 'images/lowfat-milk.png',
+    head:'Benefits of milk',
+    subHead:'Research has proven that some fruits and veggies reduces blood sugar.',
+    para:'Teaching the best and different kinds of milk to reduce blood sugar spikes... '
+  },
+  {
+    image: 'images/tubers.jpg',
+    head:'Benefits of tubers',
+    subHead:'Research has proven that some fruits and veggies reduces blood sugar.',
+    para:'Demonstrating how to count portion sizes of foods to reduce blood sugar spikes... '
+  },
+  {
+    image: 'images/green-apples.webp',
+    head:'Benefits of apples',
+    subHead:'Research has proven that some fruits and veggies reduces blood sugar.',
+    para:'Listing the best fruits and veggies with low glycemic indexes...'
+  },
+  {
+    image: 'images/image1.jfif',
+    head:'Yochai Benkler',
+    subHead:'Research has proven that some fruits and veggies reduces blood sugar.',
+    para:'Benkler studies common-based peer production, and '
+  },
+  {
+    image: 'images/oils.jpg',
+    head:'benefits of oils',
+    subHead:'Research has proven that some fruits and veggies reduces blood sugar.',
+    para:'Teaching you the top best oil to consume, and how it should be consumed... '
+  },
+];
+function featureDisplay(){
+const featureMain = document.querySelector('.box3a');
+for(let i=0;i<featureData.length; i++){
+  const cardContent = document.createElement('div');
+cardContent.classList.add('box3b');
+const imgfeature = document.createElement('img');
+imgfeature.classList.add('image');
+imgfeature.setAttribute("src",featureData[i].image);
+imgfeature.setAttribute("alt","picture of foods");
+const card = document.createElement('div');
+card.classList.add('card');
+const cardHead = document.createElement('h2');
+cardHead.innerHTML= featureData[i].head;
+const cardSubHead = document.createElement('h6');
+cardSubHead.innerHTML= featureData[i].subHead;
+const cardHr = document.createElement('hr');
+cardHr.classList.add('thirdline');
+const paraCard = document.createElement('p');
+paraCard.innerHTML= featureData[i].para;
+//appending all the elemets
+featureMain.appendChild(cardContent);
+cardContent.appendChild(imgfeature);
+cardContent.appendChild(card);
+card.appendChild(cardHead);
+card.appendChild(cardSubHead);
+card.appendChild(cardHr);
+card.appendChild(paraCard);
+}
+};
+featureDisplay(featureData);
